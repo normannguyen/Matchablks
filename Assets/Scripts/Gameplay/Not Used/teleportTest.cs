704ca18c1746ede174c4afc60d6ab044ef99b636
Norman Nguyen <normannguyen3@gmail.com>
2020-12-16 02:05:33 -0700

    organizing and working on the score.

---
+++ Assets/Scripts/Gameplay/Not Used/teleportTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class teleportTest : MonoBehaviour
+
+{
+
+    public Transform teleportTarget;
+
+    public GameObject thePlayer;
+
+
+
+    void OnTriggerEnter()
+
+    {
+
+        thePlayer.transform.position = teleportTarget.transform.position;
+
+    }
+
+}
